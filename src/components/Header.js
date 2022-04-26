fff8f924237999c7e99cc927ac24da3cf0504382
Alline <alline_franciely@hotmail.com>
2022-04-26 18:56:41 -0300

    criaçao do header - requisitos 9 a 12

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { history } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
+
+function Header() {
+  const [showSearch, setshowSearch] = useState(false);
+  return (
+    <header>
+      <button
+        type="button"
+        data-testid="profile-top-btn"
+        onClick={ () => history.push('/profile') }
+      >
+        <img
+          src={ profileIcon }
+          alt="profile icon"
+        />
+        Perfil
+      </button>
+      <h1 data-testid="page-title">Recipes App</h1>
+      <button
+        type="button"
+        data-testid="search-top-btn"
+        onClick={ () => setshowSearch(!showSearch) }
+      >
+        <img
+          src={ searchIcon }
+          alt="search icon"
+        />
+        Buscar
+      </button>
+    </header>
+  );
+}
+
+export default Header;
