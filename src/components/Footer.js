fb84198f8dbdb8b3b2f814376e6470b2a7e349bc
Joao Coelho <joaopaulomelocoelho@gmail.com>
2022-04-27 08:48:43 -0300

    footer posicionado de forma fixa, requisito 20

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
   return (
     <div>
-      <footer data-testid="footer">
-        <button type="button" data-testid="drinks-bottom-btn">Bebidas</button>
-        <button type="button" data-testid="explore-bottom-btn">Explorar</button>
-        <button type="button" data-testid="food-bottom-btn">Comidas</button>
+      <footer data-testid="footer" className="footerIcons">
+        <link rel="stylesheet" type="text/css" href="App.css" media="screen" />
+        <input
+          type="button"
+          src="src/images/drinkIcon.svg"
+          alt="drink-icon"
+          data-testid="drinks-bottom-btn"
+        />
+        <input
+          type="button"
+          src="src/images/exploreIcon.svg"
+          alt="explore-icon"
+          data-testid="explore-bottom-btn"
+        />
+        <input
+          type="button"
+          src="src/images/mealIcon.svg"
+          alt="meal-icon"
+          data-testid="food-bottom-btn"
+        />
       </footer>
     </div>
   );
