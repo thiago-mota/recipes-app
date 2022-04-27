75abbac4f7d27ca291b647619d4c7f92020cd7da
Karen Cano <karen.fernandes.cano@gmail.com>
2022-04-27 18:21:00 -0300

    Adiciona retorno api a barra de busca

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Provider from './context/Provider';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Provider>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={ Login }
+          />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,14 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
+import { apiIngredient, apiName, apiFirstLetter } from '../services/apiServices';
 
 function Provider({ children }) {
-  const [search, setSearch] = useState([]);
+  const [filterSearchInput, setFilterSearchInput] = useState({
+    filterSearchInput: {
+      searchFiltered: '',
+    },
+  });
+  const [selectedRadio, setSelected] = useState();
+  const [data, setData] = useState([]);
+
+  const handleSearchInput = ({ target }) => {
+    setFilterSearchInput({ searchFiltered: target.value });
+  };
+
+  const handleClickSearch = async () => {
+    const { searchFiltered } = filterSearchInput;
+    if (selectedRadio === 'ingredient-search-radio') {
+      const returnApi = await apiIngredient(searchFiltered);
+      setData(returnApi);
+      // console.log(returnApi);
+    }
+    if (selectedRadio === 'name-search-radio') {
+      const returnApi = await apiName(searchFiltered);
+      setData(returnApi);
+    }
+    if (selectedRadio === 'first-letter-search-radio') {
+      const returnApi = await apiFirstLetter(searchFiltered);
+      setData(returnApi);
+    }
+    if (!selectedRadio || searchFiltered.length === 0) {
+      return global.alert('Preencha corretamente os critérios da busca');
+    }
+  };
 
   const contextValue = {
-    search,
-    setSearch,
+    handleSearchInput,
+    filterSearchInput,
+    handleClickSearch,
+    selectedRadio,
+    setSelected,
+    data,
   };
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   return (
     <Context.Provider value={ contextValue }>
