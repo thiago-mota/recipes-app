8d868fd443dbf0258f2994eb719660788e1a4e27
Alline <alline_franciely@hotmail.com>
2022-04-27 18:40:02 -0300

    adiciona header nas paginas explorar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Provider from './context/Provider';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Provider from './context/Provider';
 import Login from './pages/Login';
 import Explore from './pages/Explore';
 import ExploreFoods from './pages/ExploreFoods';
@@ -10,32 +10,32 @@ import ExploreDrinks from './pages/ExploreDrinks';
 
 function App() {
   return (
-   <Provider>
-    <BrowserRouter>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          component={ Login }
-        />
-        <Route
-          exact
-          path="/explore"
-          component={ Explore }
-        />
-        <Route
-          exact
-          path="/explore/foods"
-          component={ ExploreFoods }
-        />
-        <Route
-          exact
-          path="/explore/drinks"
-          component={ ExploreDrinks }
-        />
-      </Switch>
-    </BrowserRouter>
-  </Provider>
+    <Provider>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={ Login }
+          />
+          <Route
+            exact
+            path="/explore"
+            component={ Explore }
+          />
+          <Route
+            exact
+            path="/explore/foods"
+            component={ ExploreFoods }
+          />
+          <Route
+            exact
+            path="/explore/drinks"
+            component={ ExploreDrinks }
+          />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
