69ed2a786c201e3bb2061110cd616621c2afc4c8
Joao Coelho <joaopaulomelocoelho@gmail.com>
2022-04-27 09:11:54 -0300

    redirecionamento incluído, requisito 22, 23 e 24

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,24 +6,30 @@ function Footer() {
     <div>
       <footer data-testid="footer" className="footerIcons">
         <link rel="stylesheet" type="text/css" href="App.css" media="screen" />
-        <input
-          type="button"
-          src="src/images/drinkIcon.svg"
-          alt="drink-icon"
-          data-testid="drinks-bottom-btn"
-        />
-        <input
-          type="button"
-          src="src/images/exploreIcon.svg"
-          alt="explore-icon"
-          data-testid="explore-bottom-btn"
-        />
-        <input
-          type="button"
-          src="src/images/mealIcon.svg"
-          alt="meal-icon"
-          data-testid="food-bottom-btn"
-        />
+        <a href="/drinks">
+          <input
+            type="button"
+            src="src/images/drinkIcon.svg"
+            alt="drink-icon"
+            data-testid="drinks-bottom-btn"
+          />
+        </a>
+        <a href="/explore">
+          <input
+            type="button"
+            src="src/images/exploreIcon.svg"
+            alt="explore-icon"
+            data-testid="explore-bottom-btn"
+          />
+        </a>
+        <a href="/foods">
+          <input
+            type="button"
+            src="src/images/mealIcon.svg"
+            alt="meal-icon"
+            data-testid="food-bottom-btn"
+          />
+        </a>
       </footer>
     </div>
   );
