a482cffb93a48046368a2e76f89f970ba6fd485b
Alline <alline_franciely@hotmail.com>
2022-05-02 17:40:25 -0300

    comentarios

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,9 +5,9 @@ import Footer from '../components/Footer';
 
 function Profile() {
   const history = useHistory();
-  const emailUser = localStorage.getItem('user');
+  const emailUser = localStorage.getItem('user'); // o emailUser vai pegar a chave user com a info: email:... . Em seguida é feita a desestruturação dessa chave e o JSON.parse pega a string e transforma em um objeto JS para que a chave email seja acessada e retorne apenas o seu valor.
   const { email } = JSON.parse(emailUser); // estrutura retornada sem o JSON.parse: {"email":"alline_franciely@hotmail.com"}
-  console.log(email);
+
   const handleClick = () => {
     localStorage.clear();
     history.push('/');
