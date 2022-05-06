4ea5beed80b681608d52e9578fa25ff8925c935e
Thiago Mota <th.araujomota@gmail.com>
2022-05-06 00:17:06 -0300

    Adiciona testes RecipeDetails

---
+++ src/tests/RecipeDetails.test.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import RecipeDetails from '../pages/RecipeDetails';
+import { randomMeals } from './mocks/mocks';
+
+const RECIPE_PHOTO_TESTID = 'recipe-photo';
+const RECIPE_TITLE_TESTID = 'recipe-title';
+const SHARE_BTN_TESTID = 'share-btn';
+const FAVORITE_BTN_TESTID = 'favorite-btn';
+const RECIPE_CATEGORY_TESTID = 'recipe-category';
+// const INGREDIENTS_TESTID = `${index}-ingredient-name-and-measure`;
+const INSTRUCTIONS_TESTID = 'instructions';
+const VIDEO_TESTID = 'video';
+// const RECOMENDATION_CARD_TESTID = `${index}-recomendation-card`;
+const START_RECIPE_BTN_TESTID = 'start-recipe-btn';
+
+describe('Testa a página de receitas', () => {
+  beforeEach(() => {
+    renderWithRouter(<RecipeDetails />);
+
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(randomMeals),
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Possui uma foto da receita a ser preparada.', () => {
+    const recipePhoto = screen.getByTestId(RECIPE_PHOTO_TESTID);
+    expect(recipePhoto).toBeInTheDocument();
+    expect(recipePhoto).toHaveProperty('src', randomMeals.meals[0].strMealThumb);
+  });
+
+  it('Possui um título com o nome da receita.', () => {
+
+  });
+
+  it('Possui um botão para compartilhar a receita.', () => {
+
+  });
+
+  it('Possui um botão para favoritar a receita.', () => {
+
+  });
+
+  it('Possui um texto com a categoria da receita.', () => {
+
+  });
+
+  it('Exibe os ingredientes a serem utilizados.', () => {
+
+  });
+
+  it('Possui um texto com as instruções de preparo.', () => {
+
+  });
+
+  it('Possui um card com receitas recomendadas.', () => {
+
+  });
+
+  it('Possui umbotão para iniciar o preparo.', () => {
+
+  });
+
+  // it('Apenas na tela de comida, possui um vídeo.', () => {
+
+  // });
+});
